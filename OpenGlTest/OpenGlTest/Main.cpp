b99593b2d0941533c2935a7dcfc5d1bea2d4bdbe
Jarret Shook <shook.29@outlook.com>
2015-03-25 21:44:34 -0500

    added color

--- OpenGlTest/OpenGlTest/Main.cpp
+++ OpenGlTest/OpenGlTest/Main.cpp
@@ -56,6 +56,10 @@ int main()
    while (!glfwWindowShouldClose(window))
    {
       glfwPollEvents();
+
+      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+
       glfwSwapBuffers(window);
    }
 
